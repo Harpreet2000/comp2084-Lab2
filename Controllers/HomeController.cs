51d7c58836a12c95c1946114f42a00a9623b2150
Harpreet Kaur <200425194@student.georgianc.on.ca>
2020-06-02 13:34:39 -0400

    Changed the Theme.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,6 @@ namespace Lab_2.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
