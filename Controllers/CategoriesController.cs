7191dbdbe0794ad29a4f926382bf71de867f465a
Harpreet Kaur <200425194@student.georgianc.on.ca>
2020-06-02 16:19:21 -0400

    Added a new categoriescontroller and a ViewBag of category

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string category)
+       {
+            //Store the selected product name in the viewbag container
+            //viewbag is automatically passed back to the view that loads 
+           ViewBag.category = category;
+
+            return View();
+        }
+    }
+}
